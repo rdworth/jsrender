715c309057a97376ba91c81c71983263131760d0
Richard D. Worth <rdworth@gmail.com>
2011-07-28 10:36:51 -0400

    Cut test suite down to a single regression test covering only simple property access, if and each. Highlights a current limitation that a closing tag needs to be preceded by whitespace.

--- test/tests.js
+++ test/tests.js
@@ -1,112 +1,17 @@
 
-test("empty string", function() {
+test("Property Access, If, Each", function() {
 
-	equal($.render("",
-			{}
-		),
-		""
-	);
-
-	equal($.render("",
-			{ foo: "bar" }
-		),
-		""
-	);
-
-});
-
-test("string", function() {
-
-	equal($.render("foo",
-			{}
-		),
-		"foo"
-	);
-
-	equal($.render("foo",
-			{ foo: "bar" }
-		),
-		"foo"
-	);
-
-});
-
-test("single property, string", function() {
-	equal($.render("${foo}",
-			{ foo: "bar" }
-		),
-		"bar"
-	);
-});
-
-test("single property, number", function() {
-	equal($.render("${foo}",
-			{ foo: 42 }
-		),
-		"42"
-	);
-});
-
-test("single property, boolean", function() {
-
-	// true
-	equal($.render("${foo}",
-			{ foo: true }
-		),
-		"true"
-	);
-
-	// false
-	equal($.render("${foo}",
-			{ foo: false }
-		),
-		"false"
-	);
-
-});
-
-test("single property, preserve space", function() {
-
-	equal($.render(" ${foo}",
-			{ foo: "bar" }
-		),
-		" bar"
-	);
-
-	equal($.render("${foo} ",
-			{ foo: "bar" }
-		),
-		"bar "
-	);
-
-	equal($.render("    ${foo}    ",
-			{ foo: "bar" }
-		),
-		"    bar    "
-	);
-
-});
-
-test("two properties", function() {
-	equal($.render("<li><b>${Name}</b> (${ReleaseYear})</li>",
-			{ Name: "UHF", ReleaseYear: 1989 }
-		),
-		"<li><b>UHF</b> (1989)</li>"
-	);
-});
-
-test("jagged data", function() {
-
-	equal($.render("${foo}",
-			{}
-		),
-		""
-	);
-
-	equal($.render("before${foo}after",
-			{}
-		),
-		"beforeafter"
+	equal($.render(
+		" ${foo}${bar}"
+		+ "{{if baz}}baz {{/if}}"
+		+ "{{if qux}}qux {{/if}}"
+		+ "{{each list}}${$value} {{/each}}",
+		{
+			foo: "foo",
+			baz: true,
+			list: [ "a", "b" ]
+		}),
+		"foobaz a b "
 	);
 
 });
